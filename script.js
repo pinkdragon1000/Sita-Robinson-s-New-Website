8c8604bbe27f882172f170f8123c239ec58ebe32
sitarobinson <sita.robinson@gmail.com>
2018-10-24 00:15:58 -0400

    update

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
 
-  function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides"); 
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-
-  slides[slideIndex-1].style.display = "block";
-
-}
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     
@@ -46,4 +14,6 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+
 
